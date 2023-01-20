70057959af2cf92da744c5201bbfda07a0e64d1f
Sushma Gupta <65363303+being-techie@users.noreply.github.com>
2023-01-20 14:27:06 +0530

    Update streamlit_app.py
    Adding some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit 
 streamlit.title(" New Project ")
 streamlit.title(" New Project heading ")
+streamlit.header(" New Project header ")
+streamlit.text(" Creating a new Project with Snowflake ")
